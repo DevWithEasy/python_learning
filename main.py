0bbb84bbcc5a16d7f4d8eb647e74548553a89137
DevWithEasy <devwitheasy@gmail.com>
2023-11-25 10:37:12 +0600

    install mysql

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-name = input('Enter your username : ')
-password = input('Enter your password : ')
-print(name, password)
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="sql_learn"
+)
+
+db = mydb.cursor()
+
+#Insert data into database
+"""
+sql = 'insert into users (name, email, password) values (%s, %s, %s)'
+values = ('Admin', 'admin@mail.com', 'admim')
+
+db.execute(sql, values)
+
+mydb.commit()
+
+print(db.rowcount, "record inserted.")
+"""
+
+#Get all records
+"""
+db.execute('select * from users')
+
+myresult = db.fetchall()
+
+for x in myresult:
+    print(x)
+"""
+
+db.execute('drop table replies')
+db.execute('drop table comments')
+db.execute('drop table posts')
